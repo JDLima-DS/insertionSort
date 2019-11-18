a0df1cfc366e51ff2ff07f4916683f995f59b335
João Davi Costa <52018408+Par-zy@users.noreply.github.com>
2019-11-18 16:31:37 -0300

    Create insertionSort.py

---
+++ insertionSort.py
@@ -0,0 +1,38 @@
+from random import randint as random
+
+def randlist():
+  v = []
+  sizeOf = random(3, 20)
+  for i in range(2, sizeOf):
+    v.append(random(0, 1000))
+    return v
+
+def insertSort(v):
+  for i in range(1, len(v)):
+    c = 1
+    
+    while v[c] < v[c-1]:
+      if c > 0:
+          v[c-1], v[c] = v[c], v[c-1]
+          c -= 1
+        
+      else:
+        break
+  
+  return(v)
+
+print("Digite 1 para escolher os numeros")
+print("Digite 2 para numeros aleatorios")
+mode = int(input())
+
+if mode == 1:
+  v=[]
+  n = int(input("numero de termos: "))
+  for i in range (0, n, 1):
+    x = int(input())
+    v.append(x)
+    
+  print(insertSort(v))
+elif mode == 2:
+  v = randlist()
+  print(insertSort(v))
